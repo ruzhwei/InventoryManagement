237bd01dece306451fc1d34b936ee3f52b548f03
Ruzhe Wei <weiruzhe@qq.com>
2022-05-01 21:55:36 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # InventoryManagement
- 
+
+This is for shopify technical challenge.
+
+CRUD and warehouses are implemented.
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+from os import name
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,17 +8,26 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # Create model for Products and Warehouses with the following attributes.
+# Warehouse and Product are in one-to-many relationship
 class Warehouse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-
+    def __repr__(self):
+        return self.name
+    
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product = db.Column(db.String(100), nullable=False)  # must have a name
     quantity = db.Column(db.Integer, default=1)
     comments = db.Column(db.String(200))
     ware_id = db.Column(db.Integer, db.ForeignKey("warehouse.id"))
-    #dp = db.relationship("Warehouse", backref='pers')
+    warehouse = db.relationship('Warehouse', backref=db.backref('products', lazy = 'dynamic'))
+
+    def __init__(self, product, warehouse ):
+        self.product = product
+        self.warehouse = warehouse
+    def __repr__(self):
+        return '<Products %r>' % self.product
 # Creates the SQLite database (db).
 @app.before_first_request
 def create_table():
@@ -27,24 +36,80 @@ def create_table():
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    columns = [column.name for column in Products.__mapper__.columns if column.name != "id"]
-    # Add new record to the db
+    columns = [column.name for column in Products.__mapper__.columns if column.name != "id" and column.name != "ware_id"]
+    columns.append('warehouse')
+    # Add new product to the db
     if request.method == "POST":
-        new_task = Products(**request.form)
-
+        new_warehouse_name = request.form['warehouse']
+        new_warehouse = Warehouse.query.filter_by(name = str(new_warehouse_name)).first()
+        # if not specified or the warehouse user input does not exist, warehouse will be "None", we can update this attr later   
+        new_task = Products(product = request.form['product'], warehouse= new_warehouse)
+        for column in columns:
+            if column == 'warehouse' or column == 'product':
+                continue
+            else:
+                setattr(new_task, column, request.form[column])
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect("/")
         except:
-            return "There was an issue creating your product"
+            return "There was an issue creating this product"
 
     # List all.
     else:
         
         products = Products.query.all()
-        return render_template("index.html", products=products, columns=columns)
+        warehouse = Warehouse.query.all()
+        return render_template("index.html", products=products, columns=columns, warehouse = warehouse)
 
+
+@app.route("/manageWare", methods=["POST", "GET"])
+def manageWare():
+    
+    if request.method == "POST":
+        # This is to add new warehouses
+        new_task = Warehouse(name = request.form['Warehouse Name'])
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/manageWare")
+        except:
+            return "There was an issue creating this warehouse"
+
+    # List all.
+    else:
+        warehouse = Warehouse.query.all()
+        return render_template("manageWare.html", warehouse = warehouse)
+
+@app.route("/manageWare/delete/<int:id>")
+def deleteWare(id):
+    # Delete warehouse with given id value from the db.
+    warehouse_to_delete = Warehouse.query.get_or_404(id)
+    try:
+        db.session.delete(warehouse_to_delete)
+        db.session.commit()
+        return redirect("/manageWare")
+    except:
+        return "There was an issue deleting this warehouse"
+
+@app.route("/manageWare/rename/<int:id>", methods=["POST", "GET"])
+def rename(id):
+    warehouse_to_rename = Warehouse.query.get_or_404(id)
+
+    # Rename this warehouse with given id
+    if request.method == "POST":
+        setattr(warehouse_to_rename, "name", request.form['Warehouse Name'])
+        print(warehouse_to_rename, warehouse_to_rename.name)
+        try:
+            db.session.commit()
+            return redirect("/manageWare")
+        except:
+            return "There was an issue rename this warehouse"
+
+    else:
+        return render_template("rename.html", house=warehouse_to_rename)
 
 @app.route("/delete/<int:id>")
 def delete(id):
@@ -60,22 +125,25 @@ def delete(id):
 
 @app.route("/update/<int:id>", methods=["POST", "GET"])
 def update(id):
-    columns = [column.name for column in Products.__mapper__.columns if column.name != "id"]
+    columns = [column.name for column in Products.__mapper__.columns if column.name != "id" and column.name != "ware_id"]
+    columns.append('warehouse')
     product_to_update = Products.query.get_or_404(id)
 
-    # Update a product info, then redirect to "list all".
+    # Update the info of a product.
     if request.method == "POST":
         for column in columns:
-            setattr(product_to_update, column, request.form[column])
+            if column == 'warehouse':
+                new_warehouse_name = request.form[column]
+                new_warehouse = Warehouse.query.filter_by(name = str(new_warehouse_name)).first()
+                # if not specified or the warehouse user input does not exist, warehouse will be "None", we can update this attr later   
+                product_to_update.warehouse = new_warehouse
+            else:
+                setattr(product_to_update, column, request.form[column])
         try:
             db.session.commit()
             return redirect("/")
         except:
             return "There was an issue updating that product"
 
-    # Display the update page for uses to update info.
     else:
         return render_template("update.html", product=product_to_update, columns=columns)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
